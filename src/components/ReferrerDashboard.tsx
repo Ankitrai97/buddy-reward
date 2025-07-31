b43b5923059b5e237eb5573242dfbc3856e1aef8
Ankit rai <ankitraitalks@gmail.com>
2025-07-31 12:02:19 +0530

    fixed gap

--- src/components/ReferrerDashboard.tsx
+++ src/components/ReferrerDashboard.tsx
@@ -150,17 +150,20 @@ const ReferrerDashboard = () => {
                   <CardHeader>
                     <div className="flex justify-between items-start">
                       <CardTitle className="text-xl">{referral.client_name}</CardTitle>
-                      <div className="flex gap-2">
-                        <span>Stage:</span>
-                        <Badge className={getStageColor(referral.stage)}>
-                          {referral.stage}
-                        </Badge>
-                        <span>Bonus Status:</span>
-                        <Badge className={getBonusColor(referral.bonus_status)}>
-                          {referral.bonus_status}
-                        </Badge>
-                      </div>
-                    </div>
+                    <div className="flex flex-col items-end gap-1">
+            <div className="flex items-center gap-2">
+              <span>Stage:</span>
+              <Badge className={getStageColor(referral.stage)}>
+                {referral.stage}
+              </Badge>
+            </div>
+            <div className="flex items-center gap-2">
+              <span>Bonus Status:</span>
+              <Badge className={getBonusColor(referral.bonus_status)}>
+                {referral.bonus_status}
+              </Badge>
+            </div>
+          </div>
                     <CardDescription>
                       Added on {new Date(referral.created_at).toLocaleDateString()}
                     </CardDescription>
